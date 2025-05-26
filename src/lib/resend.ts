c3ae947130c3b742f7e4bd638d9cef44eb3eb016
Logiinx <Logiinx@proton.me>
2025-05-26 21:29:53 +0200

    fix resend.ts with .env (using bun/vite not next.js)

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,3 +1,9 @@
 import { Resend } from "resend"
 
-export const resend = new Resend(process.env.RESEND_API_KEY)
+const resendKey = import.meta.env.VITE_RESEND_API_KEY
+
+if (!resendKey) {
+  console.error("Missing Resend API key")
+}
+
+export const resend = new Resend(resendKey!)
